50e44023068a01a9ca3b8678c3c4e8dfd0dc019d
Mustafa <mustafaerbilici@gmail.com>
2025-01-03 23:09:42 +0300

    first commit

---
+++ api/src/test/java/com/forum/Api/ForumRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
